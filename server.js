5a7932997b55020a34f32f21dfe2672008e557b2
Lamasu <rafaerthal@gmail.com>
2014-09-20 07:52:25 -0300

    settign default port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express, app;
 express = require('express');
 app = express();
 app.use('/', express.static(__dirname + '/www'));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 8080);
 
 module.exports = app;
